c953c8694134a92c4bcd65fc8821176a5115d2e2
Kanticha0993 <s6606021610010@email.kmutnb.ac.th>
2025-01-07 04:44:01 +0700

    2231

---
+++ asyncwait-1.js
@@ -0,0 +1,16 @@
+function promiseTimeout(ms) {
+    return new Promise((resovel,reject) => {
+        setTimeout(resovel,ms);
+    });
+}
+
+async function run () {
+    //logic
+
+    console.log("Staart!!");
+    //try take of awit and compare
+    await promiseTimeout(2000);
+    console.log("Stop!!");
+}
+
+run();
